9125497b846339c946b0eaa9bfe5b631f604b44b
Juan Manuel <jmmunoza@eafit.edu.co>
2023-03-28 17:29:55 -0500

    fixing

--- app/restore/main.py
+++ app/restore/main.py
@@ -1,5 +1,48 @@
 import json
 import sys, os, tarfile
+from cryptography.fernet import Fernet
+
+
+def decryptSegment(segment_path, fernet):
+    print('Decrypting', segment_path)
+    
+    decrypt_path = os.path.dirname(segment_path) + '/decrypt' + os.path.basename(segment_path) + 'tar.gz'
+    
+    with open(segment_path, 'rb') as segment_file:
+        with open(decrypt_path, 'wb') as decrypt_file:
+            content = segment_file.read()
+            decrypted = fernet.decrypt(content)
+            decrypt_file.write(decrypted)
+            
+            print(segment_file.name, 'decrypted as', decrypt_file.name)
+            
+            return decrypt_path
+    
+
+
+def restoreTar(input_path, output_path):
+    with open(f'{input_path}segments.json', 'r') as segments_file:
+        segments_dict = json.load(segments_file)
+        
+    with open(f'{input_path}key.key', 'rb') as key_file:
+        fernet_key = key_file.read()
+        fernet = Fernet(fernet_key)
+
+    with open(f'{output_path}restore.tar.gz', 'wb') as restoring_tar:
+        for key in sorted(segments_dict.keys()):
+            segment_path = segments_dict[key]
+            decrypted_path = decryptSegment(segment_path, fernet)
+            
+            with open(decrypted_path, 'rb') as decrypted_file:
+                content = decrypted_file.read()
+                restoring_tar.write(content)
+                os.remove(decrypted_path)
+                
+    with tarfile.open(f'{output_path}restore.tar.gz', "r:*") as tar:
+        tar.extractall(output_path)
+        
+    os.remove(f'{output_path}restore.tar.gz')
+
 
 def main():
     input_path  = sys.argv[1]
@@ -7,45 +50,10 @@ def main():
 
     if not os.path.isdir(input_path) or not os.path.isdir(output_path):
         raise ValueError('one of the inputs is not a folder')
-    
-    
-    with open(f'{input_path}parts.json', 'r') as f:
-        parts_dict = json.load(f)
-        
 
-    with open(f'{output_path}restore.tar.gz', 'wb') as restoring_tar:
-        for key in sorted(parts_dict.keys()):
-            part_file = parts_dict[key]
-            
-            with open(part_file, 'rb') as f:
-                contnet = f.read()
-                restoring_tar.write(contnet)
-                
-    with tarfile.open(f'{output_path}restore.tar.gz', "r:*") as tar:
-        tar.extractall(output_path)
-        
+    restoreTar(input_path, output_path)
     
-                
-
-    # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA RESTORE
-    
-    
-def getFolderSize(folder_path: str):
-    size = 0
-
-    for ele in os.scandir(folder_path):
-        size+=os.path.getsize(ele)
-        
-    return size
         
 if __name__ == '__main__':
     main()
-         
-        
-
-    #tar = tarfile.open(output_path + 'output.tar.gz')
-    #tar.extractall('../data/restore/')
-  
-
-
-    #tar.close()
+    
--- app/store/main.py
+++ app/store/main.py
@@ -1,6 +1,83 @@
 import json
 import sys, os, tarfile
 from cryptography.fernet import Fernet 
+import hashlib
+
+
+def generateTarfile(input_path, output_path):
+    tar_path = output_path + 'temp_.tar.gz'
+    
+    tar = tarfile.open(tar_path, "w:gz")
+    for input_file in os.scandir(input_path):
+        tar.add(input_file.path)
+    tar.close()
+    
+    print('Tarfile generated in', tar_path)
+    
+    return tar_path
+
+
+def splitTarfile(tar_path):
+    print('Spliting Tarfile...')
+    
+    with open(tar_path, 'rb') as file:
+        content = file.read()
+        
+    segments_dict = {}
+    tar_folder_path = os.path.dirname(tar_path)
+    
+    size = len(content) // 5
+    segments = [content[i:i+size] for i in range(0, len(content), size)]
+    
+    for i, segment in enumerate(segments):
+        with open(f'{tar_folder_path}/segment_{i+1}.tar.gz', 'wb') as segment_file:
+            segment_file.write(segment)
+            segments_dict[i + 1] = segment_file.name
+            print(segment_file.name, 'created!')
+            
+    with open(f'{tar_folder_path}/segments.json', 'w') as segments_file:
+        json.dump(segments_dict, segments_file, indent=4)
+        print(segments_file.name, 'JSON generated!')
+            
+    os.remove(tar_path)
+    print(tar_path, 'removed...')
+    
+    
+def createHash(filename):
+    hash_object = hashlib.md5(filename.encode())
+    return hash_object.hexdigest()
+    
+    
+def encryptSegments(output_path):
+    print('Encrypting segments...')
+    fernet_key = Fernet.generate_key()
+    fernet = Fernet(fernet_key)
+    
+    with open(f'{output_path}key.key', 'wb') as key_file:
+        key_file.write(fernet_key)
+
+    
+    with open(f'{output_path}segments.json', 'r') as segments_fie:
+        segments_dict = json.load(segments_fie)
+        print('segments.json opened')
+        
+    for key in sorted(segments_dict.keys()):
+        segment_path = segments_dict[key]
+        segment_hash_path = output_path + createHash(segment_path)  
+        with open(segment_hash_path, 'wb') as segment_encrypted:
+            with open(segment_path, 'rb') as segment_file:
+                content = segment_file.read()
+                encrypted = fernet.encrypt(content)
+                segment_encrypted.write(encrypted)
+                segments_dict[key] = segment_encrypted.name
+                print(segment_file.name, 'encrpyted as', segment_encrypted.name)
+                os.remove(segment_path)
+                
+    with open(f'{output_path}segments.json', 'w') as segments_file:
+        json.dump(segments_dict, segments_file, indent=4)
+        print(segments_file.name, 'JSON updated!')
+
+
 
 def main():
     input_path  = sys.argv[1]
@@ -9,31 +86,11 @@ def main():
     if not os.path.isdir(input_path) or not os.path.isdir(output_path):
         raise ValueError('one of the inputs is not a folder')
     
-    # Tarfile
-    tar = tarfile.open(output_path + 'temp_output.tar.gz', "w:gz")
-    for input_file in os.scandir(input_path):
-        tar.add(input_file.path)
-    tar.close()
-    
-    # Spliting the Tarfile
-    with open(output_path + 'temp_output.tar.gz', 'rb') as file:
-        content = file.read()
-        
-    # Creating Dict to export as a Json
-    parts_dict = {}
-    
-    size = len(content) // 5
-    partes = [content[i:i+size] for i in range(0, len(content), size)]
-    
-    for i, parte in enumerate(partes):
-        with open(f'{output_path}output{i+1}.tar.gz', 'wb') as new_file:
-            new_file.write(parte)
-            parts_dict[i + 1] = new_file.name
-            
-    with open(f'{output_path}parts.json', 'w') as f:
-        json.dump(parts_dict, f, indent=4)
-            
-    os.remove(output_path + 'temp_output.tar.gz')
+    print('Backup process started...')
+    tar_path = generateTarfile(input_path, output_path)
+    splitTarfile(tar_path)
+    encryptSegments(output_path)
+
     
 if __name__ == '__main__':
     main()
